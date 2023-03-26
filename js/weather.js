c60839bf5290275c6b8bd12e9b7b115e4158123e
wofn <siny10188@naver.com>
2023-03-26 16:57:52 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,11 @@
 const API_KEY = "29025953bd125d0e02783e2e92d47dd7";
+//사이트 -profile - api key
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  fetch(url)
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url) //url을 부르기
     .then((response) => response.json())
     .then((data) => {
       const city = document.querySelector("#weather span:first-child");
@@ -17,5 +18,5 @@ function onGeoOk(position) {
 function onGeoError() {
   console.log("can't find you, No weather for you.");
 }
-
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+!navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+//이걸하면 브라우저에서 위치 좌표를 알 수 있다.
