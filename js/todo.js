6e135d150b1d35455c55cab24e67bebf0f87ecf6
wofn <siny10188@naver.com>
2023-03-26 16:33:19 +0900

    update todo js

--- js/todo.js
+++ js/todo.js
@@ -5,17 +5,23 @@ const TODOS_KEY = "todos";
 
 let toDos = [];
 
+//4
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+  //JSON.stringify – 객체를 JSON으로 바꿔줍니다. 객체를 문자열로 바꿔주어요
 }
-
+//3
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  //li.id가 string이기 때문에 parseint로 문자열을 숫자로 바꿔줌
   toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  //filter - 지우고 싶은 item을 빼고 새 array 생성, 예전 array도 있고 새로 만든 것도 있는 거임
+  //filter은 filter함수가 필요하다고 우리에게 요청, 함수는 반드시 true를 리턴해야됨!!
+  //false를 리턴하면 그 item은 새 array에 포함되지 않을거다.
   saveToDos();
 }
-
+//2
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   li.id = newTodo.id;
@@ -28,7 +34,7 @@ function paintToDo(newTodo) {
   li.appendChild(button);
   toDoList.appendChild(li);
 }
-
+//1
 function hadleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
@@ -45,8 +51,9 @@ function hadleToDoSubmit(event) {
 toDoForm.addEventListener("submit", hadleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
-
+//5
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   parsedToDos.forEach(paintToDo);
+  //forEach array에 있는 각각의 item에 대해서 실행
 }
